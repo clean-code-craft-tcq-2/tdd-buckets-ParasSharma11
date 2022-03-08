cdc3adf11fb783ec08b86e66e8c1815adb1cc500
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-03-08 17:47:22 +0530

    Create testRangeChecker.java

---
+++ testRangeChecker.java
@@ -0,0 +1,16 @@
+package tdd-buckets-ParasSharma11;
+import java.util.*; 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class testRangeChecker 
+{
+  @Test
+    public void testNumbeOfReadingsFromGivenRange()
+    {
+      List<Integer> listOfvalues = new ArrayList<Integer>(); 
+      Collections.addAll(list, 10, 20, 30, 40); 
+      assertTrue(rangeChecker.get_no_of_readings_from_given_range(listOfvalues,4,5) == 2));
+    }
+}
